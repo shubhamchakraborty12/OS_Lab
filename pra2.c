9df50cc9d1af55750dbb1d4ee36005cb402088a3
Shubham Chakraborty <166355394+shubhamchakraborty12@users.noreply.github.com>
2026-01-20 12:27:58 +0530

    Add files via upload

---
+++ pra2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+int a,b,c;
+a = 59;
+b = 10;
+c = a + b;
+printf("%d",c);
+return 0;
+}
---
+++ pra3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a, b, c;
+
+printf("Enter the number: ");
+scanf("%d", &a);
+printf("Enter another number: ");
+scanf("%d", &b);
+c = a + b;
+printf("The answer is: %d", c);
+return 0;
+}
